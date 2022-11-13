01ef691da332f20fd89499aedda61b85dbd3c377
hianova <ethan90827@gmail.com>
2022-11-13 18:44:17 +0800

    1.1.6

--- data.java
+++ data.java
@@ -6,34 +6,33 @@ import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.regex.Pattern;
 
 public class data {
+    private Elements head, body;
     private final ArrayList<Integer> request;
     private final ArrayList<String>[] req_tag;
-    private Elements head, body;
+    private final boolean has_tag;
 
     public data(String in, ArrayList<String> request_in) throws Exception {
         var tag = Jsoup.parse(new File(in), "UTF-8").select("tag").text();
         var check = new checksyn();
         var parse = Jsoup.parse(new File(in), check.getTag(tag + "_encode"));
-        request = new ArrayList<>();
-        req_tag = new ArrayList[request_in.size()];
         head = parse.select(check.getTag(tag + "_head"));
         body = parse.select(check.getTag(tag + "_body"));
+        request = new ArrayList<>();
+        req_tag = new ArrayList[request_in.size()];
+        has_tag = request_in.toString().contains("#");
 
         request_in.forEach((tmp) -> {
             request.add(head.eachText().indexOf(tmp.split("#")[0]));
-            if (tmp.contains("#")) {
+            if (has_tag) {
                 req_tag[request_in.indexOf(tmp)] = new ArrayList<>();
-                for (var count = 1; count < tmp.split("#").length; count++) {
-                    req_tag[request_in.indexOf(tmp)].add(tmp.split("#")[count]);
-                }
-            } else {
-                req_tag[request_in.indexOf(tmp)] = new ArrayList<>() {{
-                    add("");
-                }};
+                if (tmp.contains("#")) {
+                    for (var count = 1; count < tmp.split("#").length; count++)
+                        req_tag[request_in.indexOf(tmp)].add(tmp.split("#")[count]);
+                } else
+                    req_tag[request_in.indexOf(tmp)].add("");
             }
         });
         if ("mops_twse_com_tw".equals(tag)) head.add(new Element("th").text("備註"));
@@ -45,22 +44,25 @@ public class data {
         var skip_list = Pattern.compile("(合計｜小計｜總計)");
 
         body.forEach((body_tmp) -> {
-            var line = new ArrayList<String>();
-            var line_check = false;
-
             if (skip_list.matcher(body_tmp.text()).find()) return;
+            var line = new ArrayList<String>();
+            var line_check = true;
+
             request.forEach((request_tmp) -> {
                 var tmp = body_tmp.child(request_tmp).select("td").text();
                 line.add(tmp.isEmpty() ? "null" : tmp);
             });
-            if (Arrays.stream(req_tag).allMatch(ArrayList::isEmpty)) {
-                for (var count_line = 0; count_line < line.size(); count_line++) {
-                    for (var count = 0; count < req_tag[count_line].size(); count++) {
-                        if (req_tag[count_line].get(count).isBlank()) break;
-                        if (line.get(count_line).contains(req_tag[count_line].get(count))) line_check = true;
+            if (has_tag) {
+                for (var count = 0; count < req_tag.length; count++) {
+                    for (var count_tag = 0; count_tag < req_tag[count].size(); count_tag++) {
+                        if (req_tag[count].get(count_tag).equals(line.get(count))) {
+                            out.addAll(line);
+                            return;
+                        }
                     }
                 }
-            } else line_check = true;
+                line_check = false;
+            }
             if (line_check) out.addAll(line);
         });
         return out;
@@ -77,19 +79,3 @@ public class data {
     }
 
 }
-
-
-//            for (var count = 0; count < req_tag.length; count++) {
-//                if (req_tag[count] == null) {
-//                    line_check = true;
-//                    break;
-//                }
-//                if (req_tag[count].get(0).isBlank()) continue;
-//                for (var count_tag = 0; count_tag < req_tag[count].size(); count_tag++) {
-//                    if (!line.get(count).contains(req_tag[count].get(count_tag))) {
-//                        line_check = true;
-//                        break;
-//                    }
-//                }
-//            }
-//            if (!line_check) return;
